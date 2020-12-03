4157d225f1389bca85f7410a3c8b1c8ebb68dffa
LorenzoMarnat <lmarnat14@gmail.com>
2020-12-03 10:40:14 +0100

    Pick building by distance with town center

--- ProceduralGeneration/Assets/Scripts/GridManager.cs
+++ ProceduralGeneration/Assets/Scripts/GridManager.cs
@@ -5,26 +5,33 @@ using UnityEngine;
 public class GridManager : MonoBehaviour
 {
     [SerializeField]
-    Transform parent;
+    Transform parent = null;
 
     [SerializeField]
-    List<GameObject> buildingPrefabs;
+    List<GameObject> buildingPrefabs = null;
 
     [SerializeField, Range(10, 200)]
     int gridSize = 10;
 
     [SerializeField]
     float buildingSize = 1f;
+
+    [SerializeField]
+    int seed = -1;
+
+    [SerializeField]
+    int pickMod = 0;
+
+    private Vector3 townCenter;
     // Start is called before the first frame update
     void Start()
     {
-        GenerateGrid();
-    }
+        townCenter = new Vector3((gridSize / 2f) * buildingSize, 0, (gridSize / 2f) * buildingSize);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (seed < 0)
+            seed = Random.Range(0, 100);
+
+        GenerateGrid();
     }
 
     private void GenerateGrid()
@@ -33,14 +40,24 @@ public class GridManager : MonoBehaviour
         {
             for (int j = 0; j < gridSize; j++)
             {
-                //int buildingToInstantiate = PickBuildingRandom();
-                int buildingToInstantiate = PickBuildingPerlinNoise(i, j);
+                int buildingToInstantiate = PickBuilding(i, j);
                 GameObject building = Instantiate(buildingPrefabs[buildingToInstantiate], new Vector3(i*buildingSize, 0, j*buildingSize), Quaternion.identity);
                 building.transform.parent = parent;
             }
         }
     }
 
+    private int PickBuilding(int i, int j)
+    {
+        switch(pickMod)
+        {
+            case 0: return PickBuildingRandom();
+            case 1: return PickBuildingPerlinNoise(i, j);
+            case 2: return PickBuildingTownCenter(i, j);
+            default: Debug.Log("Invalid picking mod");
+                return 0;
+        }
+    }
     private int PickBuildingRandom()
     {
         return Random.Range(0, buildingPrefabs.Count);
@@ -48,6 +65,24 @@ public class GridManager : MonoBehaviour
 
     private int PickBuildingPerlinNoise(int i, int j)
     {
-        return (int)(Mathf.PerlinNoise(i/10f, j/10f) * buildingPrefabs.Count);
+        return (int)(Mathf.PerlinNoise(i/10f + seed, j/10f + seed) * buildingPrefabs.Count);
+    }
+
+    private int PickBuildingTownCenter(int i, int j)
+    {
+        Vector3 pos = new Vector3(i * buildingSize, 0, j * buildingSize);
+        float distanceToCenter = Vector3.Distance(pos, townCenter);
+        float size = (gridSize * buildingSize) / 2f;
+
+        if (distanceToCenter < 0.33f * size)
+            return buildingPrefabs.Count - 1;
+        else if(distanceToCenter < 0.66f * size)
+            return buildingPrefabs.Count - 2;
+        else if (distanceToCenter < size)
+            return buildingPrefabs.Count - 3;
+        else if (distanceToCenter < 1.2f * size)
+            return buildingPrefabs.Count - 4;
+        else
+            return 0;
     }
 }
